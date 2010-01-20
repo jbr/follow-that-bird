97de7e9d1211846eb9e0bf76ae76d9ff408c670d
Jacob Rothstein <github@jacobrothstein.com>
2010-01-20 01:06:04 -0800

    some pretty output

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,16 +1,14 @@
 class Tweet < ActiveRecord::Base
-  cattr_accessor :twitter
   validates_uniqueness_of :tweet_id
   
-  def self.connect
-    auth = Twitter::HTTPAuth.new(AppConfig.twitter_username, AppConfig.twitter_password)
-    @@twitter = Twitter::Base.new auth
-  end
-  
   def self.update_from_twitter
+    count = 0
     Twitter::Search.new(Hashtag.search_string).each do |tweet|
-      Tweet.create :tweet_id => tweet.id, :text => tweet.text, :from_user => tweet.from_user
+      if Tweet.create :tweet_id => tweet.id, :text => tweet.text, :from_user => tweet.from_user
+        count += 1
+      end
     end
+    puts "added #{count} (total: #{Tweet.count})"
   end
   
   
