86e8d74be197b264aac74fa1810ef533dcb59878
Jacob Rothstein <github@jacobrothstein.com>
2010-01-20 01:03:49 -0800

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,14 +37,16 @@ namespace :deploy do
   end
 end
 
+
+#the following from http://gist.github.com/244420
 namespace :bundler do
   task :install, :roles => :app, :except => { :no_release => true }  do
     run("gem install bundler --source=http://gemcutter.org")
   end
  
   task :symlink_vendor, :roles => :app, :except => { :no_release => true } do
-    shared_gems = File.join(shared_path, 'vendor/gems/ruby/1.8')
-    release_gems = "#{release_path}/vendor/gems/ruby/1.8"
+    shared_gems = File.join(shared_path, 'vendor/bundled_gems/ruby/1.8')
+    release_gems = "#{release_path}/vendor/bundled_gems/ruby/1.8"
     %w(gems specifications).each do |sub_dir|
       shared_sub_dir = File.join(shared_gems, sub_dir)
       run("mkdir -p #{shared_sub_dir} && mkdir -p #{release_gems} && ln -s #{shared_sub_dir} #{release_gems}/#{sub_dir}")
@@ -53,7 +55,7 @@ namespace :bundler do
  
   task :bundle_new_release, :roles => :app, :except => { :no_release => true }  do
     bundler.symlink_vendor
-    run("cd #{release_path} && gem bundle --only #{rails_env} --cached")
+    run("cd #{release_path} && gem bundle --only production --cached")
   end
 end
  
