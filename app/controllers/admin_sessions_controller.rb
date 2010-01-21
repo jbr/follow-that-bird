8a7049fcbeca36a28e612f9468da7c8ef99d43aa
Jacob Rothstein <github@jacobrothstein.com>
2010-01-20 20:31:35 -0800

    admin sessions controller and a basic tweet controller

---
+++ app/controllers/admin_sessions_controller.rb
@@ -0,0 +1,14 @@
+class AdminSessionsController < ApplicationController
+  admin :denied
+  
+  def create
+    password = params[:admin_session][:password]
+    if password == AppConfig.admin_password
+      session[:password] = password
+      redirect_to root_url
+    else
+      flash[:notice] = "Incorrect password"
+      redirect_to new_admin_session_url
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
   include InheritedResources::DSL
+  
+  def admin_required
+    admin? or access_denied
+  end
+
+  def admin_denied
+    redirect_to root_url and return false if admin?
+  end
+  
+  def self.admin(action, options = {})
+    case action
+    when :required
+      before_filter :admin_required, options
+    when :denied
+      before_filter :admin_denied, options
+    end
+  end
+  
+  def access_denied
+    redirect_to new_admin_session_url and return false
+  end
+  
+  def admin?
+    session[:admin_password] == AppConfig.admin_password
+  end
 end
