c266b1d6f9350ae15a38f966ed72c1c4db52f197
Nat Budin <natbudin@gmail.com>
2010-01-23 15:08:23 -0500

    record votes

--- app/controllers/tweet_votes_controller.rb
+++ app/controllers/tweet_votes_controller.rb
@@ -1,5 +1,15 @@
 class TweetVotesController < ApplicationController
   def vote
-#    head :ok
+    @tweet = Tweet.find(params[:id])
+
+    case params[:vote]
+    when "up"
+      @tweet.add_upvote
+    when "down"
+      @tweet.add_downvote
+    end
+    
+    @tweet.save
+    head :ok
   end
 end
