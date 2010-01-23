d00ed069609d9145f2af794a498e6e1a67bef1b5
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2010-01-23 12:34:41 -0500

    better Tweet schema; storing Tweet lat/long, to_user, source, profile_image_url if available; removed migration to makes tweet_id a String; added tests on Tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,7 +8,21 @@ class Tweet < ActiveRecord::Base
   def self.update_from_twitter
     count = 0
     Twitter::Search.new(Hashtag.search_string).since(Tweet.last.try(:tweet_id) || 0).each do |tweet|
-      if Tweet.create(:tweet_id => tweet.id, :text => tweet.text, :from_user => tweet.from_user).valid?
+      if tweet.geo.present? && tweet.geo.coordinates.present?
+        latitude, longitude = *tweet.geo.coordinates
+      else
+        latitude, longitude = nil, nil
+      end
+      if Tweet.create(:tweet_id => tweet.id, 
+                      :text => tweet.text, 
+                      :from_user => tweet.from_user, 
+                      :time_of_tweet => tweet.created_at, 
+                      :to_user => tweet.to_user,
+                      :profile_image_url => tweet.profile_image_url,
+                      :source => tweet.source,
+                      :latitude => latitude,
+                      :longitude => longitude
+                      ).valid?
         count += 1
       end
     end
@@ -24,4 +38,25 @@ class Tweet < ActiveRecord::Base
       sleep pause
     end
   end
+  
+  # Getter that converts from database-version of latitude (which is multiplied by 1,000,000)
+  def latitude
+    latitude_times_1000000.nil? ? nil : latitude_times_1000000 / 1_000_000.0
+  end
+  
+  # Setter that converts to database-version of latitude (which is multiplied by 1,000,000)
+  def latitude=(lat)
+    self.latitude_times_1000000 = lat.nil? ? nil : lat * 1_000_000.0
+  end
+  
+  # Getter that converts from database-version of longitude (which is multiplied by 1,000,000)
+  def longitude
+    longitude_times_1000000.nil? ? nil : longitude_times_1000000 / 1_000_000.0
+  end
+  
+  # Setter that converts to database-version of longitude (which is multiplied by 1,000,000)
+  def longitude=(long)
+    self.longitude_times_1000000 = long.nil? ? nil : long * 1_000_000.0
+  end
+  
 end
