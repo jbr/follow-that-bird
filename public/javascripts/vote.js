d0a1aed88c7d0bc5139dae01891f869531d1fc31
James Rosen <jrosen@mitre.org>
2010-01-23 19:28:16 -0500

    showing 'done voting, vote more?' link when all Tweets voted on; changed Tweet list to an actual list

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -1,31 +1,33 @@
 var FollowThatBird = {
   voteClicked: function() {
     var tweetDiv = $(this).parents('.tweet');
-    var tweetDivId = tweetDiv.attr('id');
     var matcher = /tweet_(\d+)/;
-    var match = matcher.exec(tweetDivId);
+    var match = matcher.exec(tweetDiv.attr('id'));
     var tweetId = match[1];
 
     var vote;
     if ($(this).hasClass("up")) {
-        vote = "up";
+      vote = "up";
     } else if ($(this).hasClass("down")) {
-        vote = "down";
+      vote = "down";
     }
 
     $.ajax({
-        url: "/tweets/"+tweetId+"/"+vote,
-        type: "POST",
-        success: function () {
-            tweetDiv.hide();
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            alert("Sorry, there was an error recording your vote.  Please try again later.");
+      url: "/tweets/"+tweetId+"/"+vote,
+      type: "POST",
+      success: function () {
+        if (tweetDiv.siblings().length == 0) {
+          $('#show_more_tweets').show();  
         }
+        tweetDiv.remove();
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        alert("Sorry, there was an error recording your vote.  Please try again later.");
+      }
     });
   }
 }
 
-$(function() {
-    $('.tweet .vote').bind('click', FollowThatBird.voteClicked);
+$(document).ready(function() {
+  $('.tweet .vote').bind('click', FollowThatBird.voteClicked);
 });
