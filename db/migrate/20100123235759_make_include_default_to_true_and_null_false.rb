23a34b7f319fcd5a77618da3257bec7f0b560448
Jacob Rothstein <github@jacobrothstein.com>
2010-01-25 15:20:29 -0800

    make sure the no-null migration runs happily on mysql & sqlite

--- db/migrate/20100123235759_make_include_default_to_true_and_null_false.rb
+++ db/migrate/20100123235759_make_include_default_to_true_and_null_false.rb
@@ -1,6 +1,6 @@
 class MakeIncludeDefaultToTrueAndNullFalse < ActiveRecord::Migration
   def self.up
-    execute "update hashtags set include = 'false' where include is null"
+    execute "update hashtags set include = 0 where include is null"
     change_column :hashtags, :include, :boolean, :default => true, :null => false
   end
 
